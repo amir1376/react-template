959e841d853c96a2bf7be62ca9175e236575e4cb
amirhosseinshammari <62371264+hosseinSh1379@users.noreply.github.com>
2025-02-18 15:23:29 +0330

    upgrade style and dependencies (#1)
    * upgrade dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve:{
-    alias:{
-      "~":"./src"
+    plugins: [
+        react(),
+    ],
+    resolve: {
+        alias: {
+            "~": path.resolve(".", "src")
+        }
+    },
+    server: {
+        port: 3000,
+        host: "0.0.0.0"
     }
-  },
-  server:{
-    port:3000
-  }
 })
